86ffc926c2bd9f89e6d9e3a93fe44a20af5861f5
Anshul Anand <ianshulanand@gmail.com>
2025-07-16 11:43:59 +0530

    Updated settings

--- webboard/settings.py
+++ webboard/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^0quth+3)^-lnk92u_woum2i)=c@o32nv&ht#wk2&ybr7ti$zl'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'webboard.onrender.com']
 
